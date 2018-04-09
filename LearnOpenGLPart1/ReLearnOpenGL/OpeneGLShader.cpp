9b5a5ed08b027b7e9f632351790b54ee89f44e9c
AverJing <panda12396@163.com>
2018-04-09 15:22:22 +0800

    Add files via upload

---
+++ LearnOpenGLPart1/ReLearnOpenGL/OpeneGLShader.cpp
@@ -0,0 +1,143 @@
+/*
+*
+*@author£ºAver Jing
+*@description£ºshader extension
+*@date£ºApril 4, 2018
+*
+*/
+
+#include<iostream>
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include "Shader.h"
+
+using namespace std;
+
+//setting
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+
+//function declaration
+void frameBuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() {
+	//glfw initialize and configure
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//glfw window creation
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hello", NULL, NULL);
+	if (!window) {
+		cout << "Failed to create GLFW window. " << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//glad: load all opengl funtion pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed  to initialize GLAD. " << endl;
+		return -1;
+	}
+
+	//vertex attributes
+	//float vertices[] = {
+	//	//first triangle
+	//	0.5f, 0.5f, 0.0f,// top right
+	//	0.5f, -0.5f, 0.0f,//buttom right
+	//	-0.5f, 0.5f, 0.0f,//top tight
+	//	//second triangle
+	//	0.5f, -0.5f, 0.0f,  // buttom right
+	//	-0.5f, -0.5f, 0.0f, // buttom left
+	//	-0.5f, 0.5f, 0.0f   // top left
+
+	//};
+	float vertices[] = {
+		//position		 //color
+		0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f,// top right
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,//buttom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,// buttom left
+		-0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top left
+
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,//first triangle
+		1, 2, 3//second triangle
+	};
+
+	//set VBO
+	unsigned int VBO, VAO, EBO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//link vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glfwSetFramebufferSizeCallback(window, frameBuffer_size_callback);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	Shader shader("shader.vs", "shader.fs");
+
+
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//Input
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw our first triangle
+		shader.use();
+
+		// update uniform color
+		/*float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);*/
+
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		//glfw: swap buffers and poll IO evenets
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	//de-allocate all resources once they've outlived their purpose
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
+	//glfw: teminate, cleaing all previously allocated GLFW resources
+	glfwTerminate();
+
+	return 0;
+}
+//process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+//glfw: whenever the window size changed this callback function executes.
+void frameBuffer_size_callback(GLFWwindow* window, int width, int height) {
+	//
+	glViewport(0, 0, width, height);
+}
